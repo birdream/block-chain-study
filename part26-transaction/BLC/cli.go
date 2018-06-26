b162a7baa23e0710811b93890bbe6f358b973cdf
nomran <nochen@xogrp.com>
2018-06-26 18:44:46 +0800

    single trasaction

---
+++ part26-transaction/BLC/cli.go
@@ -0,0 +1,110 @@
+package BLC
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math/big"
+	"os"
+	"time"
+)
+
+type CLI struct {
+	BC *Blockchain
+}
+
+func (cli *CLI) printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("    addBlock -data BLOCK_DATA")
+	fmt.Println("    printchain - ")
+}
+
+func (cli *CLI) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		os.Exit(1)
+	}
+}
+
+func (cli *CLI) sendToken() {
+	tx1 := NewUTXOTransaction("Norman", "Jan", 10, cli.BC)
+	tx2 := NewUTXOTransaction("Norman", "Lu", 5, cli.BC)
+
+	cli.BC.MineBlock([]*Transaction{tx1, tx2})
+}
+
+func (cli *CLI) printChain() {
+	var blockchanIterator *BlockchainIterator
+	blockchanIterator = cli.BC.Iterator()
+
+	var hashInt big.Int
+
+	for {
+		// fmt.Printf("%x\n", blockchanIterator.CurrentHash)
+		block := blockchanIterator.Next()
+
+		fmt.Printf("prevHash: %x \n", block.PrevBlockHash)
+		fmt.Printf("Timestamp: %s \n", time.Unix(block.Timestamp, 0))
+		fmt.Printf("Hash: %x \n", block.Hash)
+		fmt.Printf("Transactions: %x \n", block.Transactions)
+		fmt.Printf("Nonce: %d \n", block.Nonce)
+		for _, tranx := range block.Transactions {
+			fmt.Printf("Transactions: %x\n", tranx.ID)
+		}
+		fmt.Printf("\n")
+
+		hashInt.SetBytes(blockchanIterator.CurrentHash)
+
+		if hashInt.Cmp(big.NewInt(0)) == 0 {
+			break
+		}
+	}
+}
+
+func (cli *CLI) addBlock(data string) {
+	count, outputMap := cli.BC.FindSpendableOutputs("Norman", 5)
+
+	fmt.Println(count)
+	fmt.Println(outputMap)
+	cli.sendToken()
+}
+
+/*
+ hello
+*/
+func (cli *CLI) Run() {
+	cli.validateArgs()
+
+	addBlockCmd := flag.NewFlagSet("addBlock", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+	addBlockData := addBlockCmd.String("data", "", "Block Data")
+
+	switch os.Args[1] {
+	case "addblock":
+		err := addBlockCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "printchain":
+		err := printChainCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	default:
+		cli.printUsage()
+		os.Exit(1)
+	}
+
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			addBlockCmd.Usage()
+			os.Exit(1)
+		}
+		// fmt.Println("data:" + )
+		cli.addBlock(*addBlockData)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
+}
---
+++ part27-transaction/BLC/blockchain.go
@@ -0,0 +1,225 @@
+package BLC
+
+import (
+	"encoding/hex"
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/boltdb/bolt"
+)
+
+const dbFile = "blockchain.db"
+const blocksBucket = "blocks"
+const genesisCoinbaseData = "The Time 03/jan/2009 chancellor on brink of second bailout for banks"
+
+type Blockchain struct {
+	Tip []byte   //最后一个区块的hash
+	DB  *bolt.DB // 数据库
+}
+
+// 新增区块
+
+// func (Blockchain *Blockchain) AddBlock(data string) {
+// 	// 创建新block
+// 	prevBlock := Blockchain.Blocks[len(Blockchain.Blocks)-1]
+
+// 	newBlock := NewBlock(data, prevBlock.Hash)
+
+// 	// 将区块添加到Blocks里面
+// 	Blockchain.Blocks = append(Blockchain.Blocks, newBlock)
+// }
+
+// 创建一个带有创世区块的区块链
+func NewBlockChain(address string) *Blockchain {
+	var tip []byte //获取最后一个区块的HASH
+
+	db, err := bolt.Open(dbFile, 0600, nil)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// defer db.Close()
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		// 表
+		b := tx.Bucket([]byte(blocksBucket))
+
+		if b == nil {
+			fmt.Println("No existing blockchain found, Creating a new one")
+			cbtx := NewCoinBaseTX(address, genesisCoinbaseData)
+			genesis := NewGeneisBlock(cbtx)
+
+			// 创建表
+			b, err = tx.CreateBucket([]byte(blocksBucket))
+			if err != nil {
+				log.Panic(err)
+			}
+
+			// 创建创世区块
+			err = b.Put(genesis.Hash, genesis.Serialize())
+			if err != nil {
+				log.Panic(err)
+			}
+
+			err = b.Put([]byte("l"), genesis.Hash)
+			if err != nil {
+				log.Panic(err)
+			}
+
+			tip = genesis.Hash
+		} else {
+			tip = b.Get([]byte("l"))
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &Blockchain{tip, db}
+}
+
+func (blockchain *Blockchain) AddBlock(transactions []*Transaction) {
+	newBlock := NewBlock(transactions, blockchain.Tip)
+
+	err := blockchain.DB.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+
+		err := b.Put(newBlock.Hash, newBlock.Serialize())
+		if err != nil {
+			log.Panic(err)
+		}
+
+		err = b.Put([]byte("l"), newBlock.Hash)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		blockchain.Tip = newBlock.Hash
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+// 返回当前用户未花费输出的所有交易的集合并返回交易的数组
+func (bc *Blockchain) FindUnspentTransactions(address string) []Transaction {
+	var unspentTXs []Transaction
+	spentTXOs := make(map[string][]int)
+
+	bci := bc.Iterator()
+	var hashInt big.Int
+	for {
+		block := bci.Next()
+
+		// fmt.Printf("prevHash: %x \n", block.PrevBlockHash)
+		// fmt.Printf("Timestamp: %s \n", time.Unix(block.Timestamp, 0))
+		// fmt.Printf("Hash: %x \n", block.Hash)
+		// fmt.Printf("Transactions: %x \n", block.Transactions)
+		// fmt.Printf("Nonce: %d \n", block.Nonce)
+		// for _, tranx := range block.Transactions {
+		// 	fmt.Printf("Transactions: %x\n\n", tranx.ID)
+		// }
+		for _, tran := range block.Transactions {
+			// fmt.Printf("Transactions: %x", tran.ID)
+			// fmt.Print(tran)
+			txID := hex.EncodeToString(tran.ID)
+
+		Outputs:
+			for outIdx, out := range tran.Vout {
+				//是否已经花费
+				if spentTXOs[txID] != nil {
+					for _, spentOut := range spentTXOs[txID] {
+						if spentOut == outIdx {
+							continue Outputs
+						}
+					}
+				}
+
+				if out.CanBeUnlockedWith(address) {
+					unspentTXs = append(unspentTXs, *tran)
+				}
+			}
+
+			if tran.IsCoinbase() == false {
+				for _, in := range tran.Vin {
+					if in.CanUnlockOutputWith(address) {
+						inTxId := hex.EncodeToString(in.Txid)
+						spentTXOs[inTxId] = append(spentTXOs[inTxId], in.Vout)
+					}
+				}
+			}
+		}
+
+		hashInt.SetBytes(bci.CurrentHash)
+
+		if hashInt.Cmp(big.NewInt(0)) == 0 {
+			break
+		}
+	}
+
+	return unspentTXs
+}
+
+// 查找可用的未花费的输出信息
+func (bc *Blockchain) FindSpendableOutputs(address string, amount int) (int, map[string][]int) {
+	unspentOutputs := make(map[string][]int)
+
+	unspentTXs := bc.FindUnspentTransactions(address)
+
+	accumulated := 0
+
+Work:
+	for _, tx := range unspentTXs {
+		txID := hex.EncodeToString(tx.ID)
+
+		for outIdx, out := range tx.Vout {
+			if out.CanBeUnlockedWith(address) && accumulated < amount {
+				accumulated += out.Value
+				unspentOutputs[txID] = append(unspentOutputs[txID], outIdx)
+
+				if accumulated >= amount {
+					break Work
+				}
+			}
+		}
+	}
+
+	return accumulated, unspentOutputs
+}
+
+// 根据交易的数组 打包新的区块
+func (bc *Blockchain) MineBlock(txs []*Transaction) {
+	err := bc.DB.Update(func(tx *bolt.Tx) error {
+		// 新建区块
+		newBlock := NewBlock(txs, bc.Tip)
+
+		b := tx.Bucket([]byte(blocksBucket))
+
+		if b != nil {
+			err := b.Put(newBlock.Hash, newBlock.Serialize())
+			if err != nil {
+				log.Panic(err)
+			}
+
+			err = b.Put([]byte("l"), newBlock.Hash)
+			if err != nil {
+				log.Panic(err)
+			}
+
+			bc.Tip = newBlock.Hash
+		}
+
+		// 将区块存储到数据库
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+}
